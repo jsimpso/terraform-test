3d83c9cf5278e528f650b696b32455e578a0fa75
James Simpson <james.simpson@canonical.com>
2023-11-07 23:10:07 +0200

    Remove providers and variables

--- providers.tf
+++
@@ -1,7 +0,0 @@
-provider "juju" {
-  controller_addresses = var.juju_controller_addresses
-  username             = var.juju_username
-  password             = var.juju_password
-  ca_certificate       = var.juju_ca_certificate
-}
-
