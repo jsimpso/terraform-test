11592fc474d9feb0c8029ce51fd1dbb0f65c1b34
James Simpson <james.simpson@canonical.com>
2023-11-09 09:20:46 +0200

    Revert to basic config

--- main.tf
+++ main.tf
@@ -1,10 +1,3 @@
-provider "juju" {
-  controller_addresses = "192.168.20.99:17070"
-  ca_certificate       = var.juju_ca_cert
-  username             = "admin"
-  password             = var.juju_password
-}
-
 resource "juju_model" "development" {
   name = "development"
 
--- variables.tf
+++
@@ -1,8 +0,0 @@
-variable "juju_password" {
-  type = string
-  sensitive = true
-}
-
-variable "juju_ca_cert" {
-  type = string
-}
